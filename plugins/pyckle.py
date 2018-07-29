39999efb345553da63953f6791e9abf52ec6c590
AtkLordOverAll <darkninjabuni@gmail.com>
2018-07-29 20:40:46 +0100

    Pyckle plugin added, clone of Pyson but for pickle

---
+++ plugins/pyckle.py
@@ -0,0 +1,29 @@
+import os
+import pickle
+
+class Pyckle():
+
+    """
+    Allows for easier manipulation of pickles.
+    If a pickle isn't found data will be None.
+    """
+
+    def __init__(self, fileName):
+        if not fileName.endswith(".pickle"):
+            fileName += ".pickle"
+        if not os.path.isfile(fileName):
+            data = None
+            print("File not found, Pyckle blank")
+        else:
+            try:
+                with open(fileName) as f:
+                    data = pickle.load(f)
+            except ValueError:
+                data = None
+                print("Data read failed, Pyckle blank")
+        self.fileName = fileName
+        self.data = data
+
+    def save(self):
+        with open(self.fileName, "w") as f:
+            pickle.dump(self.data, f, protocol=pickle.HIGHEST_PROTOCOL)
